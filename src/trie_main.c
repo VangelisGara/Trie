32820dde7214fd9b6849928c724abc9f9e77625f
devme <developpmentt@gmail.com>
2019-08-01 23:06:45 +0300

    init commit

---
+++ src/trie_main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/trie.h"
+#include "../include/postlist.h"
+
+/* NOTES */
+/* ========================================================================== */
+/*    1.gdb --args ./minisearch -i ./inputs/doc1.txt     	                  */
+/*    2.valgrind --leak-check=full -v ./minisearch -i ./inputs/bigDataset.txt */
+/* ========================================================================== */
+
+extern int trieSize; // global variable that stores trie size
+
+int main(){
+  char buffer[500];
+
+  trieNode* root = NULL; // trie resides here
+  trie_insert(&root,"award",1);
+  trie_insert(&root,"acab",2);
+  trie_insert(&root,"arizona",3);
+  trie_insert(&root,"awe",4);
+
+  printTrie(root,buffer,0);
+  trie_destroy(root);
+}
