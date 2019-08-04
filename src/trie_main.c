4d813b73175ce84dfb61be3bc2962d75e2ce7c8a
devme <developpmentt@gmail.com>
2019-08-05 01:05:00 +0300

    added some comments

--- src/trie_main.c
+++ src/trie_main.c
@@ -4,12 +4,6 @@
 #include "../include/trie.h"
 #include "../include/postlist.h"
 
-/* NOTES */
-/* ========================================================================== */
-/*    1.gdb --args ./minisearch -i ./inputs/doc1.txt     	                  */
-/*    2.valgrind --leak-check=full -v ./minisearch -i ./inputs/bigDataset.txt */
-/* ========================================================================== */
-
 extern int trieSize; // global variable that stores trie size
 
 int main(){
@@ -17,11 +11,12 @@ int main(){
 
   trieNode* root = NULL; // trie resides here
   trie_insert(&root,"award",1);
+  trie_insert(&root,"award",1);
   trie_insert(&root,"acapella",2);
   trie_insert(&root,"acapella",5);
   trie_insert(&root,"arizona",3);
   trie_insert(&root,"awe",4);
-
+  // print trie words and the # of documents each word exists in
   printTrie(root,buffer,0);
   trie_destroy(root);
 }
